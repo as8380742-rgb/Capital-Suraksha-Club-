b8e69d44caffb005bd47ed8fbb230a167661cc35
as8380742-rgb <as8380742@gmail.com>
2026-03-19 21:37:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app.secret_key = "CSC_ULTRA_FIXED_V2"
 
 # --- CONFIGURATION ---
 # Bhai, yahan apni REAL API KEY dalo bina * ke
-FAST2SMS_KEY = 'plwd********************' 
+FAST2SMS_KEY = 'plwdy58v3eLJWFKNcS0mksbBMHuxRhDIAPqaQfUY16TECig7oZ8FPoGwcg15XuAWZmfUhKOq3dijsM7x' 
 YOUR_WHATSAPP = "918287550979"
 YOUR_UPI = "8587965337-1@nyes"
 
